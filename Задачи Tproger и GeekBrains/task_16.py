7bc0b4e393262092fb8d118569b26b6fd166ad15
ItYaS Woker <70561906+ItYaS@users.noreply.github.com>
2021-03-07 18:02:26 +0700

    Delete task_16.py

--- Задачи Tproger и GeekBrains/task_16.py
+++
@@ -1,7 +0,0 @@
-# незнаю что это значит и как сделать
-# я просто скопировал код с Tproger
-# 19.04.2020
-from os import listdir
-from os.path import isfile, join
-files = [f for f in listdir('/home') if isfile(join('/home', f))]
-print(files)
