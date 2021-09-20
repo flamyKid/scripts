ae18d31b4b53e9c0fda66e44f1472ef9e191f791
Seraphim Ryaboshapko <70561906+ityas@users.noreply.github.com>
2021-09-20 11:50:59 +0700

    Delete problem_001.py

--- project_euler/old_script/problem_001.py
+++
@@ -1,4 +0,0 @@
-num = list(range(0, 1000)) # список с числами от 0 до 1000
-result = list() # список с числами которые делятся на 3 и 5
-
-print(list([elem for elem in num if elem%3==0 and elem%5==0]))
